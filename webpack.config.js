f7d21936ff6efa966d09b9d8b546c6c182b55162
Evan Moncuso <evan.moncuso@gmail.com>
2016-08-04 12:06:53 -0700

    Add framework of frontend, webpack set up, npm things

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: './app/scripts/index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node-modules/,
+        loader: [
+          'babel-loader',
+        ],
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node-modules/,
+        loader: [
+          'babel-loader'
+        ],
+        query: {
+          presets: ['react']
+        }
+      }
+    ],
+  },
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  plugins:[HTMLWebpackPluginConfig]
+};
