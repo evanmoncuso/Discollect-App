8acfee5fd1c2c8937e0c6e85197b7e5c9e8d8fdf
Evan Moncuso <evan.moncuso@gmail.com>
2016-08-05 18:17:55 -0700

    Refactor categories and main item listings

--- server/user/userModel.js
+++ server/user/userModel.js
@@ -5,7 +5,7 @@ var saltRounds = 10;
 
 var User = db.define('user', {
   username: {type: Sequelize.STRING(20), unique: true},
-  password: {type: Sequelize.STRING(20)},
+  password: {type: Sequelize.STRING(100)},
   email: Sequelize.STRING,
   rating: Sequelize.INTEGER,
   picReference: Sequelize.STRING,
