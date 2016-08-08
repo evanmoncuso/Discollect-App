2335be1bee5ef9cad967cf948a2b9a8b3c799768
Jordan Chong <jordanchong23@gmail.com>
2016-08-08 15:36:49 -0700

    Merge

--- app/scripts/components/Dashboard.jsx
+++ app/scripts/components/Dashboard.jsx
@@ -2,21 +2,55 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 
-const Dashboard = ({ username, valid }) => (
-  <div>
-    <div className="main_container">
-      <h1>Dashboard</h1>
-      <img src="https://huxley.wwu.edu/sites/huxley.wwu.edu/files/default_images/user-icon.png" alt="user" />
-      <div className="about">{valid ? username : 'NOT A VALID USER'}</div>
+class Dashboard extends React.Component {
+  constructor(props) {
+    // props: username, valid
+    super(props);
+    this.state = {
+      tableView: true,
+      activeListings: [],
+      pendingOrders: [],
+      listingsHistory: [],
+      ordersHistory: [],
+    };
+    this.toggleTableView = this.toggleTableView.bind(this);
+  }
+  toggleTableView() {
+    this.setState({
+      tableView: !this.state.tableView,
+    });
+  }
+  render() {
+    return (
       <div>
-        <div className="activeListings">activeListings</div>
-        <div className="pendingOrders">pendingOrders</div>
-        <div className="listingsHistory">listingsHistory</div>
-        <div className="ordersHistory">ordersHistory</div>
+        <div className="main_container">
+          <h1>Dashboard</h1>
+          <img src="https://huxley.wwu.edu/sites/huxley.wwu.edu/files/default_images/user-icon.png" alt="user" />
+          <div className="about">{this.props.valid ? this.props.username : 'NOT A VALID USER'}</div>
+          <br />
+          <h3>Acount:</h3>
+          <button onClick={this.toggleTableView}>{this.state.tableView ? 'Show History' : 'Show Active'}</button>
+          <table>
+            <thead>
+              <tr>
+                <td>{this.state.tableView ? 'Active' : 'Past'} Listings</td>
+                <td>{this.state.tableView ? 'Pending' : 'Past'} Orders</td>
+              </tr>
+              <tr>
+                <td>{[1, 2, 3].map((a) => {
+                  return <tr><td>{a}</td></tr>;
+                })}</td>
+                <td>{['a', 'b', 'c'].map((a) => {
+                  return <tr><td>{a}</td></tr>;
+                })}</td>
+              </tr>
+            </thead>
+          </table>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 const mapStateToProps = (state) => (
   {
@@ -38,3 +72,8 @@ Dashboard.propTypes = {
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+
+    // <div className="activeListings">activeListings</div>
+    // <div className="pendingOrders">pendingOrders</div>
+    // <div className="listingsHistory">listingsHistory</div>
+    // <div className="ordersHistory">ordersHistory</div>
