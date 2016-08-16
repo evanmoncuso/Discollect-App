f123eb6cfd311ba4217c755bce0c298ee835ba39
Cradanimal <cradwimb@gmail.com>
2016-08-15 17:01:48 -0700

    (bug) change picRef check to !picRef

--- app/scripts/actions/itemActions.js
+++ app/scripts/actions/itemActions.js
@@ -37,7 +37,7 @@ const itemActions = {
     (dispatch) => {
       const photoUrl = 'http://photohelper.herokuapp.com/api/createNewListing';
       const url = baseUrl + '/api/createNewListing';
-      if (listingData.picReference === undefined) {
+      if (!listingData.picReference) {
         dispatch(itemActions.postListingAfterPhoto(listingData));
       } else {
         var photoData = {
