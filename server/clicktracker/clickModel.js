9bb70452e7b27e9d31bfb025fb014b24c92d9d9f
Evan Moncuso <evan.moncuso@gmail.com>
2016-08-16 19:40:15 -0700

    Add click counter to db and server middleware

---
+++ server/clicktracker/clickModel.js
@@ -0,0 +1,17 @@
+var Sequelize = require('sequelize');
+var db = require('../config/database.js');
+var User = require('../user/userModel.js');
+var Listing = require('../listing/listingModel.js');
+
+var clickModel = db.define('item_clicks', {
+  userId: Sequelize.INTEGER,
+  listingId: Sequelize.INTEGER,
+  location: Sequelize.INTEGER,
+});
+
+clickModel.belongsTo(User, {foreignKey: 'userId', targetKey: 'id'});
+clickModel.belongsTo(Listing, {foreignKey: 'listingId', targetKey: 'id'});
+
+clickModel.sync({ force: false });
+
+module.exports = clickModel;
