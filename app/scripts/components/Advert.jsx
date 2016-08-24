feca028340520094b895a302640d62c887d6f81e
Evan Moncuso <evan.moncuso@gmail.com>
2016-08-23 17:22:09 -0700

    Remove console logs and make eslint happy

--- app/scripts/components/Advert.jsx
+++ app/scripts/components/Advert.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { browserHistory, Link } from 'react-router';
 
+const Advert = ({ zipcode }) => (
+  <div>
+    <div className="adverttext">
+      Need help collecting?
+    </div>
+    <div>
+      <img
+        className="advertimg" src="http://bigboystoysrentals.com/wp-content/uploads/2014/04/u-haul-10foot.jpg"
+        alt="advertisement"
+        onClick={() => {
+          window.open("https://www.google.com/#q=u+haul+near+" + zipcode);
+        }}
+      />
+      <img
+        className="advertimg" src="http://bigboystoysrentals.com/wp-content/uploads/2014/04/u-haul-10foot.jpg"
+        alt="advertisement"
+        onClick={() => {
+        window.open("https://www.budgettruck.com/locations/ca/" + zipcode);
+        }}
+      />
+    </div>
+  </div>
+);
 
-let Advert = ({zipcode}) => {
-console.log('hello zipcode: ', zipcode)
-  return (
-        <div>
-          <div className='adverttext'>
-            Need help collecting? 
-            </div>
-            <div>
-              <img className='advertimg' src='http://bigboystoysrentals.com/wp-content/uploads/2014/04/u-haul-10foot.jpg' onClick={() => {
-              window.open('https://www.google.com/#q=u+haul+near+' + zipcode);
-              }}/>
-              <img className='advertimg' src='http://bigboystoysrentals.com/wp-content/uploads/2014/04/u-haul-10foot.jpg' onClick={() => {
-              window.open('https://www.budgettruck.com/locations/ca/' + zipcode);
-              }}/>
-            </div>
-        </div>
-  );
+Advert.propTypes = {
+  zipcode: React.PropTypes.number,
 };
 
-
-
 export default Advert;
-
--- app/scripts/components/Listing.jsx
+++ app/scripts/components/Listing.jsx
@@ -49,45 +49,45 @@ const Listing = ({ currentListing, zipcode, userId, userName, dispatchListingSta
   }
 
   return (
-  <div>
-    <div className="main_container listing_container">
-      <h2> {currentListing.title} </h2>
-      <div className="listing_content">
-        <div className="image_pane">
-          <img src={pic} alt="listing" />
+    <div>
+      <div className="main_container listing_container">
+        <h2> {currentListing.title} </h2>
+        <div className="listing_content">
+          <div className="image_pane">
+            <img src={pic} alt="listing" />
+          </div>
+          <div className="listing_description">
+            {currentListing.description}
+          </div>
+          <div className="info_pane">
+            <div className="info_condition">
+              <span className="title">Condition: </span>
+              <span className="data">{condish[currentListing.condition]}</span>
+            </div>
+            <div className="info_giver">
+              <span className="title">given by: </span>
+              <span className="data">{currentListing.username}</span>
+            </div>
+            <div className="info_zipcode">
+              <span className="title">zipcode: </span>
+              <span className="data">{currentListing.zipcode}</span>
+            </div>
+          </div>
         </div>
-        <div className="listing_description">
-          {currentListing.description}
-        </div>
-        <div className="info_pane">
-          <div className="info_condition">
-            <span className="title">Condition: </span>
-            <span className="data">{condish[currentListing.condition]}</span>
-          </div>
-          <div className="info_giver">
-            <span className="title">given by: </span>
-            <span className="data">{currentListing.username}</span>
-          </div>
-          <div className="info_zipcode">
-            <span className="title">zipcode: </span>
-            <span className="data">{currentListing.zipcode}</span>
-          </div>
+        <div className="button_container">
+          {dibsButton}
+          <button
+            className="listing_view_button"
+            onClick={() => backer()}
+          >
+            BACK
+          </button>
         </div>
       </div>
-      <div className="button_container">
-        {dibsButton}
-        <button
-          className="listing_view_button"
-          onClick={() => backer()}
-        >
-          BACK
-        </button>
+      <div className="advert">
+        <Advert zipcode={zipcode} />
       </div>
     </div>
-    <div className='advert'>
-      <Advert zipcode={zipcode}/>
-    </div>
-  </div>
   );
 };
 
@@ -98,21 +98,21 @@ Listing.propTypes = {
   dispatchListingStatusChange: React.PropTypes.func,
 };
 
-const mapStateToProps = (state) => {
-  return {
+const mapStateToProps = (state) => (
+  {
     currentListing: state.items.current,
     userId: state.users.id,
     zipcode: state.users.zip,
-  };
-};
+  }
+);
 
 const mapDispatchToProps = (dispatch) => (
   {
     dispatchListingStatusChange: (listingID) => {
-      dispatch(itemActions.updateListingStatus(listingID))
+      dispatch(itemActions.updateListingStatus(listingID));
     },
     dispatchGetListing: (listingID) => {
-      dispatch(itemActions.getIndividualListing(listingID))
+      dispatch(itemActions.getIndividualListing(listingID));
     },
   }
 );
