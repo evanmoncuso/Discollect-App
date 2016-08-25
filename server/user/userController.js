9a150308e7bafef0593b44e316ffe3584d2bc219
Evan Moncuso <evan.moncuso@gmail.com>
2016-08-25 16:54:31 -0700

    Make ESLINT happy in userController

--- server/user/userController.js
+++ server/user/userController.js
@@ -1,16 +1,13 @@
-var User = require('./userModel.js');
+const User = require('./userModel.js');
 
 module.exports = {
-
-  updateTakerRating: function(req, res) {
-    console.log(req.body.takerId)
+  updateTakerRating: (req, res) => {
     User.findOne({
       where: {
         id: req.body.takerId,
       },
     })
     .then((user) => {
-      console.log('doing the rating workings for the taker/user: ',user.username)
       const oldTotal = Number(user.avgRating) * Number(user.ratingCount);
       const newTotal = Number(oldTotal) + Number(req.body.rating);
       const newCounter = 1 + Number(user.ratingCount);
@@ -18,17 +15,16 @@ module.exports = {
       user.update({
         avgRating: newAverage,
         ratingCount: newCounter,
-      })
+      });
       res.send();
     })
     .catch((err) => {
-      console.log('updateTakerRating issue')
+      console.log('updateTakerRating issue');
       res.status(400).send(err);
     });
   },
 
-  updateGiverRating: function(req, res) {
-    console.log(req.body.takerId)
+  updateGiverRating: (req, res) => {
     User.findOne({
       where: {
         id: req.body.giverId,
@@ -42,42 +38,41 @@ module.exports = {
       user.update({
         avgRating: newAverage,
         ratingCount: newCounter,
-      })
-      res.send()
+      });
+      res.send();
     })
     .catch((err) => {
-      console.log('updateGiverRating issue')
+      console.log('updateGiverRating issue');
       res.status(400).send(err);
     });
   },
 
-  getUserInfo : function(req, res) {
-    let userId = req.user.id || req.query.id;
-    if (userId){
-      User.findOne({where : { id : userId}})
+  getUserInfo: (req, res) => {
+    const userId = req.user.id || req.query.id;
+    if (userId) {
+      User.findOne({ where: { id: userId } })
         .then(user => {
           res.json(user);
         });
-      } else{
-        res.json({user: null});
-      }
+    } else {
+      res.json({ user: null });
+    }
   },
 
-  postUser : function(req, res) {
-    User.findOrCreate({where : { username : req.body.username}})
+  postUser: (req, res) => {
+    User.findOrCreate({ where: { username: req.body.username } })
       .spread((user, created) => {
-        var message = {
-          user: user,
-          created : created
+        const message = {
+          user,
+          created,
         };
         res.json(message);
       });
   },
 
-  //login handles login and, if successful, returns zipcode with res.send for our
-  //use in zipcodeArrayBuilder
-  login: function(req, res) {
-    var message = {
+// login handles login and, if successful, returns zipcode with res.send for our
+  login: (req, res) => {
+    const message = {
       id: req.user.id,
       username: req.user.username,
       email: req.user.email,
@@ -87,69 +82,62 @@ module.exports = {
     res.json(message);
   },
 
-  signup: function(req, res) {
-    // console.log('hit on api/signup');
+  signup: (req, res) => {
     User.findOne({
-      where: {username: req.body.username}
+      where: { username: req.body.username },
     })
-    .then(function(user) {
+    .then((user) => {
       if (!user) {
         User.create(req.body)
-        .then(function(user) {
-          var message = {
+        .then((user) => {
+          const message = {
             id: user.id,
             username: user.username,
-            zipcode: user.zipcode
+            zipcode: user.zipcode,
           };
           res.json(message);
-        })
+        });
       } else {
-        console.error('That username is already in use.');
-        res.status(200).send('That username is already in use.')
+        res.status(200).send('That username is already in use.');
       }
-    })
+    });
   },
 
-  logout: function(req, res) {
+  logout: (req, res) => {
     req.logout();
-    req.session.destroy(function(err) {
-      // console.log('in teh part to destroy sesh')
+    req.session.destroy(() => {
       res.send();
-    })
+    });
   },
 
-  userProfile: function(req, res) {
+  userProfile: (req, res) => {
     User.findOne({
       where: {
         id: req.body.userID,
       },
     })
-    .then(function(user){
-      // console.log('^^^^^',user);
+    .then((user) => {
       res.send(user);
     })
-    .catch(function(err){
+    .catch((err) => {
       console.log(err);
     });
   },
 
-  updatePic: function(req, res) {
-    // console.log(req.body);
+  updatePic: (req, res) => {
     User.findOne({
       where: {
         id: req.body.userId,
       },
     })
-    .then(user => {
-      // console.log(user);
-      return user.update({
+    .then((user) => (
+      user.update({
         picReference: req.body.picReference,
-      });
-    })
-    .then((user)=>{
+      })
+    ))
+    .then((user) => {
       user.password = null;
       res.send(user);
     });
-  }
-
+  },
 };
