dddf005546e8d890ce1ebad079a8637ef9c7ad33
David Lundgren <dealundgren@gmail.com>
2016-08-26 11:51:20 -0700

    loading gif stored

--- app/scripts/components/CreateListing.jsx
+++ app/scripts/components/CreateListing.jsx
@@ -114,7 +114,7 @@ const CreateListing = ({ dispatchCreateNewListing,  id }) => {
           <div className="button_container">
             <button type="submit" className="blue_button">Create</button>
           </div>
-            <img id='giphy' src='https://prepaid.sprint.com/img/loading.gif'/>
+            <img id='giphy' src='./loading.gif'/>
         </form>
       </div>
     </div>
